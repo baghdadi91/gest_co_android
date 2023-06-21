b68e2cb92b759b2308a1669ea0563db7417a4950
baghdadi91 <kawtar.baghdadi@gmail.com>
2023-06-21 14:27:37 +0200

    Mise a jour du theme de l'application

--- app/src/main/java/fr/doranco/flash/MainActivity.java
+++ app/src/main/java/fr/doranco/flash/MainActivity.java
@@ -12,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SpecialiteDatabaseTest test = new SpecialiteDatabaseTest(this);
-        test.start();
+
+//        SpecialiteDatabaseTest test = new SpecialiteDatabaseTest(this);
+//        test.start();
     }
 }
