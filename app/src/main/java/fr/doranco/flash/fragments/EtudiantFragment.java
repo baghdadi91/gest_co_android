fc698c6510928688c51f443578252e54181ac37f
baghdadi91 <kawtar.baghdadi@gmail.com>
2023-06-22 12:18:16 +0200

    Implémentation navigation view et toolBar

---
+++ app/src/main/java/fr/doranco/flash/fragments/EtudiantFragment.java
@@ -0,0 +1,33 @@
+package fr.doranco.flash.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.doranco.flash.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link EtudiantFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EtudiantFragment extends Fragment {
+
+
+
+    public EtudiantFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_etudiant, container, false);
+    }
+}
